3566afd486641ebf2f614996cb103f531b2791b2
Julian Ornelas <julian_ornelas45@protonmail.com>
2023-08-11 13:53:43 -0400

    Mostly got the dark mode ready.

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function dark_mode() {
-    var obj = document.getElementsByClassName("header-section");
+    var obj = document.getElementsByClassName("basis");
     for (var i = 0; i < obj.length; i++) {
-        obj[i].style.backgroundColor = "#960200";
+        obj[i].style.backgroundColor = "#0F080B";
+    }
+
+    var obj = document.getElementsByClassName("body-bg");
+    for (var i = 0; i < obj.length; i++) {
+        obj[i].style.backgroundColor = "#000A11";
+    }
+
+    var obj = document.getElementsByClassName("primary");
+    for (var i = 0; i < obj.length; i++) {
+        obj[i].style.color = "#DBE1B1";
+    }
+
+    var obj = document.getElementsByClassName("secondary");
+    for (var i = 0; i < obj.length; i++) {
+        obj[i].style.color = "#FE9F9B";
     }
 }
 

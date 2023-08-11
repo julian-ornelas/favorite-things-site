b01165a7acda80c228cf18eb15fd5d6963ef72f1
Julian Ornelas <julian_ornelas45@protonmail.com>
2023-08-11 12:57:50 -0400

    Basis for dark mode made.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function dark_mode() {
+    var obj = document.getElementsByClassName("header-section");
+    for (var i = 0; i < obj.length; i++) {
+        obj[i].style.backgroundColor = "#960200";
+    }
+}
+
+
 console.log("Nothing's here. :)");

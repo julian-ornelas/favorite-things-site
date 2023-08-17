38beb8c3c49a98f2831351e58e3819840d07a21b
Julian Ornelas <julian_ornelas45@protonmail.com>
2023-08-17 14:45:54 -0400

    Made the dark mode code toggable.

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
+var isDarkMode = false;
+
 function dark_mode() {
+    isDarkMode = !isDarkMode;
+    
     var obj = document.getElementsByClassName("basis");
     for (var i = 0; i < obj.length; i++) {
-        obj[i].style.backgroundColor = "#0F080B";
+        obj[i].style.backgroundColor = isDarkMode ? "#0F080B" : ""; // Set back to original or dark mode color
     }
 
     var obj = document.getElementsByClassName("body-bg");
     for (var i = 0; i < obj.length; i++) {
-        obj[i].style.backgroundColor = "#000A11";
+        obj[i].style.backgroundColor = isDarkMode ? "#000A11" : ""; // Set back to original or dark mode color
     }
 
     var obj = document.getElementsByClassName("primary");
     for (var i = 0; i < obj.length; i++) {
-        obj[i].style.color = "#DBE1B1";
+        obj[i].style.color = isDarkMode ? "#DBE1B1" : ""; // Set back to original or dark mode color
     }
 
     var obj = document.getElementsByClassName("secondary");
     for (var i = 0; i < obj.length; i++) {
-        obj[i].style.color = "#FE9F9B";
+        obj[i].style.color = isDarkMode ? "#FE9F9B" : ""; // Set back to original or dark mode color
     }
 }
-
 
 console.log("Nothing's here. :)");
